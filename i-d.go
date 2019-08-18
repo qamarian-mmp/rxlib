7c0f670c16a0a199d84e4593abae011a79a07b95
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-08-18 03:05:19 +0100

    Progress 2

--- i-d.go
+++
@@ -1,7 +0,0 @@
-package rxlib
-
-// This type is the data type of the initialization function of mopthreads.
-type InitFn func (string, string, *<-chan *Message, *chan<- *Message, byte, byte)
-
-// This type is the data type of the deinitialization function of mopthreads.
-type DnitFn func ()
