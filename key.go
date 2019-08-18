e8fae879ee8b58c961ae60b13568121cc8029824
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-08-18 04:11:35 +0100

    Progress 3

---
+++ key.go
@@ -0,0 +1,28 @@
+package rxlib
+
+type Key interface {
+
+	State (byte, string)
+
+	StartupFailed (string)
+
+	NowRunning ()
+
+	Send (interface {}, string) error
+
+	Read (interface {}, error)
+
+	Shutdown ()
+
+	ShutdownIssued () bool
+
+	MarkShutdown (string)
+}
+
+var (
+	StateStartup         byte = 0
+	StateStartupFailure  byte = 1
+	StateRunning         byte = 2
+	StateError           byte = 3
+	StateDone            byte = 4
+)
