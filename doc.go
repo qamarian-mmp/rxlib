a9e2b090d00a8f0bcd91be7d42e444a33e51fc7a
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-08-21 15:09:52 +0100

    Progress 9

---
+++ doc.go
@@ -0,0 +1,4 @@
+// This package is Rexa's core library (http://github.com/qamarian-mmp/rexa). The contents
+// of this package, are all core to Rexa. The package majorly contains data types used by
+// Rexa and the mains of a rexa-based software (RbS).
+package rxlib
