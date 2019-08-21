b0fb73ea5e84631b43e34c6b05ee9c41f1a99c84
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-08-21 17:57:27 +0100

    Progress 11

---
+++ rxLog.go
@@ -0,0 +1,20 @@
+package rxlib
+
+// RxLog is an abstract data type. It serves as Rexa's log. Although, rexa has a default
+// implementation (emissary) of this ADT, feel free to create another implementation
+// (emissary) of this ADT, and replace the default with whatever you create.
+type RxLog interface {
+
+	// Record () could be used to add a new record to the log. The first input
+	// should be the string you want to add, while the second input should be the
+	// type of record you are adding. See the variable section for possible values
+	// of the second input.
+	Record (string, byte) (error)
+}
+
+var (
+	// Log record types
+	LrtStandard byte = 0 // The could be used to represent a normal log.
+	LrtWarning  byte = 1 // The could be used to represent a warning log.
+	LrtError    byte = 2 // The could be used to represent an error log.
+)
