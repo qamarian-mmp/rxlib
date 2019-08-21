7c9d938467f36aa87656168c33434eddcd72ace5
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-08-21 23:34:45 +0100

    Progress 14

--- register.go
+++ register.go
@@ -1,10 +1,14 @@
 package rxlib
 
+func NewRegister (id string, dep []string, startupFunc func ()) (*Register) {
+	return &Register {id, dep, startupFunc}
+}
+
 // Register is a data type that could be used to register a main with rexa.
 type Register struct {
 	id string // The ID of the main.
 	dep []string // The IDs of the main's dependencies.
-	startupFunc func () // The startup function of the main.
+	startupFunc func (Key) // The startup function of the main.
 }
 
 func (r *Register) ID () (string) {
@@ -15,6 +19,6 @@ func (r *Register) Dep () ([]string) {
 	return r.dep
 }
 
-func (r *Register) StartupFunc () (func ()) {
+func (r *Register) StartupFunc () (func (Key)) {
 	return r.startupFunc
 }
